1856e6da91ca046dd795f3c2b76bae3020b849e5
WooJin Kong <kongwoojin03@gmail.com>
2024-04-27 14:04:56 +0900

    feat: Limit crawler working time to 09:00 ~ 21:00

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from rocketry import Rocketry
-from rocketry.conditions.api import time_of_week
+from rocketry.conditions.api import time_of_week, time_of_day
 from rocketry.conds import every
 
 import sys
@@ -10,13 +10,14 @@ sys.path.append(path.abspath('.'))
 from app.crawler.v3.main import main_crawler
 from app.firebase.init_firebase import init_firebase
 
-
 app = Rocketry(execution="async")
 
 
 # Don't run the crawler on weekends
 # Because notices are not updated on weekends
-@app.task(every('3 hours', based="finish") & time_of_week.between("Mon", "Fri"))
+@app.task(
+    every('3 hours', based="finish") & time_of_day.between("09:00", "21:00") & time_of_week.between("Mon", "Fri")
+)
 async def crawler():
     await main_crawler()
 
@@ -24,7 +25,7 @@ async def crawler():
 # Run this crawler on weekends
 # Because notices are not updated on weekends
 # This crawler is for missing notice's of weekdays
-@app.task(every('12 hours', based="finish") & time_of_week.between("Sat", "Sun"))
+@app.task(every('12 hours', based="finish") & time_of_day.between("09:00", "21:00") & time_of_week.between("Sat", "Sun"))
 async def weekend_crawler():
     await main_crawler()
 
